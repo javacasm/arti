39d86221f282464ea8a2bb54f46fc31fb7ae68a2
javacasm <javacasm@gmail.com>
2015-01-19 01:27:29 +0100

    version 0.5 controls

--- articulacion.h
+++ articulacion.h
@@ -5,6 +5,10 @@ class articulacion
 {
 protected:
   
+  int iMinControl=0;
+  int iMaxControl=1023;
+
+
   int iMinPos;
   int iMaxPos;
   int iPinServo;
@@ -26,11 +30,23 @@ protected:
 
   };
   
-  
+  int getMin()
+  {return iMinPos;};
+
+  int getMax()
+  {return iMaxPos;};
+
   int setMiddle()
   {return setPosicion((iMinPos+iMaxPos)/2);};
 
-  int setPosicion(int iPos) {
+  int convertControlToPosition(int iControl)
+  {return map(iControl,iMinControl,iMaxControl,iMinPos,iMaxPos);};
+
+  int setControl(int iControl)
+  {return setPosicion(convertControlToPosition(iControl));};
+
+  int setPosicion(int iPos) 
+  {
 
   	if((iPos<iMaxPos)&&(iPos>iMinPos))
   	{
